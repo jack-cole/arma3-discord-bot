3c35b7fd4e42cf21da08a8c3131ae9664f3ee341
Jack Cole <jack-cole@users.noreply.github.com>
2019-06-28 23:05:45 -0700

    Initial

---
+++ index.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const SECRETS = require('./secrets.js')
+
+
+
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+        msg.reply('pong')
+    }
+})
+
+
+client.login(SECRETS.BOT_TOKEN);
